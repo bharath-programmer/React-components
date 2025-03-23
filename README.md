e3eb592289e5dfffa875ccf5cbcf043e7440f528
Bharathkumar Pachaiyappan(Gislen Software) <bharathkumarp@gislen.com>
2025-03-23 18:18:22 +0530

    Added textbox image in readme

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ This repository contains the react components. (under development)
 <hr  />
 
 Available components : 
-1. text box 
+1. Text box 
+
+![alt text](./public/image.png)
+<hr  />
--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <div style={{padding:10}}>
       <h3>1. Text box :</h3>
-      <TextBox textBoxValue={textValue} setTextBoxValue={setTextValue} labelText="Username" placeholderText="React" />
+      <TextBox textBoxValue={textValue} setTextBoxValue={setTextValue} labelText="User name" placeholderText="Username" />
     </div>
   )
 }
 
-export default App
+export default App;
